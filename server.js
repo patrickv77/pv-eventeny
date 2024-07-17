a711ae2b98e20b53009e45c4218d312ee9a935f6
Patrick Vuong <patrickv77@gmail.com>
2024-07-17 12:48:44 -0500

    add models and controllers folder, add hello route to test server response

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-
+  if(req.url === '/hello' && req.method === 'GET'){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.end('<h1>Hello World 2</h1>');
+  }else{
+    res.writeHead(404, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({message: 'Route Not Found'}));
+  }
   
 });
 
