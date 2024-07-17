227655b85da6f86030adc0e692931efff0a08d11
Patrick Vuong <patrickv77@gmail.com>
2024-07-17 15:06:07 -0500

    add skeleton of main controller file

---
+++ controllers/foodventenyController.js
@@ -0,0 +1,17 @@
+// const db = require('..models/database') or sequelize
+
+const foodventenyController = {};
+
+foodventenyController.getAllApps = async () => {
+
+}
+
+foodventenyController.getUserApps = async () => {
+
+}
+
+foodventenyController.updateAppStatus = async () => {
+
+}
+
+module.exports = foodventenyController;
