e4cdca65ad3584afc0f2dbbcbb0a440134b9864e
Patrick Vuong <patrickv77@gmail.com>
2024-07-18 10:57:07 -0500

    checkrole function, mock data

--- controllers/foodventenyController.js
+++ controllers/foodventenyController.js
@@ -1,9 +1,55 @@
 // const db = require('..models/database') or sequelize
 
+const db = [
+  {
+    username: 'pat1',
+    role: 'admin'
+  },
+  {
+    username: 'pat2',
+    role: 'user'
+  },
+  {
+    username: 'pat3',
+    role: 'user'
+  }
+]
+
+const dbApps = [
+  {
+    type: 'table',
+    user: 'pat2'
+  },
+  {
+    type: 'foodtruck',
+    user: 'pat2'
+  },
+  {
+    type: 'foodtruck',
+    user: 'pat3'
+  }
+]
+
+
+// functions to be used locally
+function checkRole(username) {
+  for(let user of db) {
+    if(user.username === username) return user.role;
+  }
+  
+  return "ERROR: User not found"
+}
+
+// functions to be exported
 const foodventenyController = {};
 
-foodventenyController.getAllApps = async () => {
+foodventenyController.getAllApps = async (req, res) => {
+  try {
+    
 
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 foodventenyController.getUserApps = async () => {
@@ -15,3 +61,5 @@ foodventenyController.updateAppStatus = async () => {
 }
 
 module.exports = foodventenyController;
+
+console.log(checkRole('pat1'));
