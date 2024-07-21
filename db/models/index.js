4db3525d30721540049f8d17fe94bb91ff97cfec
Patrick Vuong <patrickv77@gmail.com>
2024-07-20 19:44:43 -0500

    edit connection string in sequelize init code

--- db/models/index.js
+++ db/models/index.js
@@ -9,12 +9,13 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../config/config.js')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+let sequelize = new Sequelize('postgres://postgres:postgres@foodventeny_db:5432/foodventeny');
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
 fs
   .readdirSync(__dirname)
