be7796e83e374da773cf7f2a8ecf6d0efd2c21a7
Patrick Vuong <patrickv77@gmail.com>
2024-07-22 15:11:08 -0500

    add model for application table

---
+++ db/models/application.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class application extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  user.init({
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+      references: {
+        model: {
+          tableName: 'users'
+        },
+        key: 'id',
+      }
+    },
+    vendor_space: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      }
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'application',
+  });
+  return application;
+};
