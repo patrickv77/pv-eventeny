27eb5b37081d314765792ed8eb51523235af6b74
Patrick Vuong <patrickv77@gmail.com>
2024-07-22 15:10:59 -0500

    edit capitalization

--- db/models/user.js
+++ db/models/user.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class user extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -33,5 +33,5 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'user',
   });
-  return User;
+  return user;
 };
