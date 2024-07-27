78b12ead5521f653aabd24010b00a98b8e53322e
Patrick Vuong <patrickv77@gmail.com>
2024-07-26 19:39:06 -0500

    edit getApps to change the data being sent to the frontend, add get app template function

--- controllers/foodventenyController.js
+++ controllers/foodventenyController.js
@@ -1,4 +1,4 @@
-const { db, user, application } = require('../db/models');
+const { db, user, application, app_template } = require('../db/models');
 const bcrypt = require('bcryptjs');
 
 let salt = bcrypt.genSaltSync(10);
@@ -23,87 +23,88 @@ foodventenyController.verifyUser = async (req, res, next) => {
     // check password
     const compare = bcrypt.compareSync(password, foundUser.password);
 
-    if(foundUser === null){
+    if (foundUser === null) {
       // TODO: deal with user not found here
       return res.status(401).json('User not found');
-    }else if(!compare){ 
+    } else if (!compare) {
       // TODO: deal with password not matching
       return res.status(401).json('Wrong password');
-    }else{
+    } else {
       res.locals.userRole = foundUser.role;
-      res.locals.userID = foundUser.id;
+      res.locals.username = foundUser.username;
     }
 
     return next();
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 foodventenyController.addUser = async (req, res, next) => {
   const { username, password, password2, role } = req.body;
-  
+
   const registrationErrors = [];
 
-  const foundUser = await user.findOne({ where: { username: username } })
+  const foundUser = await user.findOne({ where: { username: username } });
 
-  if(foundUser !== null) {
-    registrationErrors.push({ message: "Username already exists" })
+  if (foundUser !== null) {
+    registrationErrors.push({ message: 'Username already exists' });
   }
 
-  if(!username || !password || !password2 || !role){
-    registrationErrors.push({ message: "Please enter all fields" });
+  if (!username || !password || !password2 || !role) {
+    registrationErrors.push({ message: 'Please enter all fields' });
   }
 
-  if(password !== password2) {
-    registrationErrors.push({ message: "Passwords do not match" }); 
+  if (password !== password2) {
+    registrationErrors.push({ message: 'Passwords do not match' });
   }
 
-  if(registrationErrors.length > 0) {
+  if (registrationErrors.length > 0) {
     res.render('register', { registrationErrors });
   } else {
     const hashword = await hashPass(password);
-    console.log(hashword)
+    console.log(hashword);
 
-    try { 
-      await user.create({ username: username, password: hashword, role: role});
-  
+    try {
+      await user.create({ username: username, password: hashword, role: role });
+
       return next();
     } catch (error) {
       console.log(error);
     }
   }
-}
+};
 
 foodventenyController.getApps = async (req, res, next) => {
   try {
     const dbApps = await application.findAll();
 
-    for(app of dbApps) {
-      const tempUser = await user.findOne({ where: { id: app.user_id}});
+    for (app of dbApps) {
+      const tempUser = await user.findOne({ where: { id: app.user_id } });
+      const vendor = await app_template.findOne({ where: { id: app.vendor_space } });
+
       app.user_id = tempUser.username;
+      app.vendor_space = vendor.vendor_type;
     }
 
-    if(res.locals.userRole === 'admin'){
-      
+    if (res.locals.userRole === 'admin') {
       res.locals.appArray = dbApps;
       return next();
-    } else if (res.locals.userRole === 'user'){
+    } else if (res.locals.userRole === 'user') {
       const userAppArray = [];
-      for(let app of dbApps) {
-        if(app.user_id === res.locals.userID) userAppArray.push(app);
+      for (let app of dbApps) {
+        if (app.user_id === res.locals.username) userAppArray.push(app);
       }
 
       res.locals.appArray = userAppArray;
       return next();
-    }else{
-      return "ERROR: Not a valid role"
+    } else {
+      return 'ERROR: Not a valid role';
     }
-
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 foodventenyController.updateAppStatus = async (req, res, next) => {
   try {
@@ -111,20 +112,24 @@ foodventenyController.updateAppStatus = async (req, res, next) => {
     const { status } = req.body;
 
     await application.update({ status: status }, { where: { id: id } });
-    
-    return 'Success'
+
+    return 'Success';
   } catch (error) {
-    console.log(error); 
+    console.log(error);
   }
+};
+
+foodventenyController.getAppTemplates = async (req, res, next) => {
+  const appsList = await app_template.findAll();
+
+  res.locals.appTemplatesList = appsList;
+
+  return next();
 }
 
-foodventenyController.createApplication = async (req, res, next) => {
+foodventenyController.createApplicationTemplate = async (req, res, next) => {};
 
-}
-
-foodventenyController.addApplication = async (req, res, next) => {
-  
-}
+foodventenyController.submitApplication = async (req, res, next) => {};
 
 module.exports = foodventenyController;
 
