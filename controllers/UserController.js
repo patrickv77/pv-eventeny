240650d2751b219f4e36e5b9893b02467be16e50
Patrick Vuong <patrickv77@gmail.com>
2024-07-31 16:50:10 -0500

    add UserController, refactored to include dependency injections and use class syntax

---
+++ controllers/UserController.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcryptjs');
+
+class UserController {
+  constructor(UserService/*, AuthorizationService*/) {
+    this.UserService = UserService;
+    // this.AuthorizationService = AuthorizationService;
+  }
+
+  addUser = async (req, res, next) => {
+    const { username, password, password2, role } = req.body;
+
+    try {
+      const foundUser = await this.UserService.findUser(username);
+
+      const registrationErrors = this.UserService.setRegistrationErrors(foundUser, username, password, password2, role);
+
+      if (registrationErrors.length > 0) {
+        res.render('register', { registrationErrors });
+      } else {
+        // const encryptedPassword = this.AuthorizationService(password);
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+
+        const newUser = await this.UserService.createUser( username, encryptedPassword, role );
+  
+        return res.status(201).json(newUser);
+      }
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+  }
+}
+
+module.exports = UserController;
