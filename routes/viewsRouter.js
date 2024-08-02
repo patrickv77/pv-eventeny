0d5b601d90ee6004daa5728581c9af1655e063ac
Patrick Vuong <patrickv77@gmail.com>
2024-08-02 12:35:06 -0500

    clean up unused routes, move routes that handle logic to other routers

--- routes/viewsRouter.js
+++ routes/viewsRouter.js
@@ -1,10 +1,7 @@
 const express = require('express');
 
-const foodventenyController = require('../controllers/foodventenyController');
-
 const router = express.Router();
 
-// views routes
 router.get('/', (req, res) => {
   res.render('index');
 });
@@ -15,39 +12,6 @@ router.get('/register', (req, res) => {
 
 router.get('/login', (req, res) => {
   res.render('login');
-});
-
-router.get('/dashboard',
-  foodventenyController.getApps,
-  (req, res) => {
-    const dash = {};
-    dash.applicationList = res.locals.appArray;
-    dash.role = req.user.role;
-
-    res.render('dashboard', { dash });
-});
-
-router.get('/adminAppTemplates',
-  foodventenyController.getAppTemplates, 
-  (req, res) => {
-  const appTemplatesList = res.locals.appTemplatesList;
-
-  res.render('adminAppTemplates', { appTemplatesList })
-});
-
-router.get('/submitApp', foodventenyController.getVendorTypes, (req, res) => {
-  const vendorTypes = res.locals.vendorTypesList;
-  res.render('submitApp', { vendorTypes });
-});
-
-router.get('/newTemplate', (req, res) => {
-  res.render('newTemplate');
-})
-
-router.get('/logout', (req, res) => {
-  req.session.destroy((err) => {
-    res.redirect('/login');
-  });
 });
 
 module.exports = router;
