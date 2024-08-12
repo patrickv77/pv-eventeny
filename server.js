1d62c0413aec718776fe323f6f81fd104312b95f
Patrick Vuong <patrickv77@gmail.com>
2024-08-12 11:53:44 -0500

    add css to some views, add css import to express, fix some css

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.set('view engine', 'ejs');
  */
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/public', express.static('public'));
 
 /**
  * Session middleware configuration.
